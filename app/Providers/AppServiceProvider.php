dff04e294119fcf946cbce8fc7a9b36e0370a699
eXeCUT <sexecut@gmail.com>
2021-04-21 16:00:12 +0300

    fixed bug with https detection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $httpsHosts = [
+            'http://books-app.test',
+            'http://books-app.execut.co',
+        ];
+        $currentUrl = URL::current();
+
+        foreach ($httpsHosts as $httpsHost) {
+            if (strpos($currentUrl, $httpsHost) === 0) {
+                URL::forceScheme('https');
+            }
+        }
         //
     }
 }
